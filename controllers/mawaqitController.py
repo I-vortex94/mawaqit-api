82fc8d6909989a7815443dbc30e73a1a1e20ab78
I-vortex94 <131229765+I-vortex94@users.noreply.github.com>
2025-06-27 00:26:18 +0200

    Update mawaqitController.py

--- controllers/mawaqitController.py
+++ controllers/mawaqitController.py
@@ -33,3 +33,7 @@ def get_year_calendar(masjid_id: str):
 def get_month_calendar(masjid_id: str, month_number: int):
     month_dict = script.get_month(masjid_id, month_number)
     return jsonable_encoder(month_dict)
+
+@router.get("/trmnl", summary="Formatted data for TRMNL")
+def get_trmnl_format():
+    return script.get_trmnl_data("mosquee-sahaba-creteil")
