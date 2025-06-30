9c49a7eed82c0851c3bdf2ac05363b2b6a991f53
I-vortex94 <131229765+I-vortex94@users.noreply.github.com>
2025-06-30 19:13:29 +0200

    Update mawaqitController.py

--- controllers/mawaqitController.py
+++ controllers/mawaqitController.py
@@ -1,14 +1,16 @@
-from fastapi import APIRouter
+# controllers/mawaqitController.py
+from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
-
+from datetime import datetime, timedelta
 from typing import List
 
 import scraping.script as script
+from scraping.script import fetch_mawaqit, get_email_hadith
 import models.models as models
 
 router = APIRouter(prefix="/api/v1")
 
-@router.get("/", summary="Greetings",)
+@router.get("/", summary="Greetings")
 def read_root():
     return {"Greetings": "Hello and Welcome to this Api, this api use the mawaqit.net as data source of prayers time in more than 8000 masjid, this api can be used to fetch data in json, you can find our docs on /docs. "}
 
@@ -22,12 +24,10 @@ def get_prayer_times(masjid_id: str):
     prayer_times = script.get_prayer_times_of_the_day(masjid_id)
     return prayer_times
 
-
 @router.get("/{masjid_id}/calendar", status_code=200, summary="get the year calendar of the prayer times")
 def get_year_calendar(masjid_id: str):
     r = script.get_calendar(masjid_id)
     return {"calendar": r}
-
 
 @router.get("/{masjid_id}/calendar/{month_number}", status_code=200, summary="get the month calendar of the prayer times", response_model=List[models.PrayerTimes])
 def get_month_calendar(masjid_id: str, month_number: int):
