5293cde73768a4664485037fa7151a907c27c64a
I-vortex94 <131229765+I-vortex94@users.noreply.github.com>
2026-03-28 09:44:35 +0100

    Update mawaqitController.py

--- controllers/mawaqitController.py
+++ controllers/mawaqitController.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter
 from fastapi.encoders import jsonable_encoder
-
 from typing import List
 
 import scraping.script as script
@@ -11,37 +10,58 @@ import pytz
 
 router = APIRouter(prefix="/api/v1")
 
-@router.get("/", summary="Greetings",)
-def read_root():
-    return {"Greetings": "Hello and Welcome to this Api, this api use the mawaqit.net as data source of prayers time in more than 8000 masjid, this api can be used to fetch data in json, you can find our docs on /docs. "}
 
-@router.get("/{masjid_id}/", status_code=200, summary="get the raw data from mawaqit website")
-def get_raw_data(masjid_id: str):
-    r = script.fetch_mawaqit(masjid_id)
-    paris_time = datetime.now(pytz.timezone("Europe/Paris")).strftime("%H:%M")
+@router.get("/", summary="Greetings")
+def read_root():
     return {
-        "rawdata": r,
-        "time": paris_time
+        "Greetings": "Hello and Welcome to this Api, this api use the mawaqit.net as data source of prayers time."
     }
 
 
-@router.get("/{masjid_id}/prayer-times", status_code=200, summary="get the prayer times of the current day", response_model=models.PrayerTimes)
+@router.get("/{masjid_id}/", status_code=200, summary="get raw data")
+def get_raw_data(masjid_id: str):
+    try:
+        r = script.fetch_mawaqit(masjid_id)
+        paris_time = datetime.now(pytz.timezone("Europe/Paris")).strftime("%H:%M")
+
+        return {
+            "rawdata": r,
+            "time": paris_time
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
+
+
+@router.get("/{masjid_id}/prayer-times", response_model=models.PrayerTimes)
 def get_prayer_times(masjid_id: str):
-    prayer_times = script.get_prayer_times_of_the_day(masjid_id)
-    return prayer_times
+    try:
+        return script.get_prayer_times_of_the_day(masjid_id)
+    except Exception as e:
+        return {"error": str(e)}
 
 
-@router.get("/{masjid_id}/calendar", status_code=200, summary="get the year calendar of the prayer times")
+@router.get("/{masjid_id}/calendar")
 def get_year_calendar(masjid_id: str):
-    r = script.get_calendar(masjid_id)
-    return {"calendar": r}
+    try:
+        r = script.get_calendar(masjid_id)
+        return {"calendar": r}
+    except Exception as e:
+        return {"error": str(e)}
 
 
-@router.get("/{masjid_id}/calendar/{month_number}", status_code=200, summary="get the month calendar of the prayer times", response_model=List[models.PrayerTimes])
+@router.get("/{masjid_id}/calendar/{month_number}", response_model=List[models.PrayerTimes])
 def get_month_calendar(masjid_id: str, month_number: int):
-    month_dict = script.get_month(masjid_id, month_number)
-    return jsonable_encoder(month_dict)
+    try:
+        month_dict = script.get_month(masjid_id, month_number)
+        return jsonable_encoder(month_dict)
+    except Exception as e:
+        return {"error": str(e)}
 
-@router.get("/{masjid_id}/trmnl", summary="Formatted data for TRMNL")
+
+@router.get("/{masjid_id}/trmnl")
 def get_trmnl_format(masjid_id: str):
-    return script.get_trmnl_data(masjid_id)
+    try:
+        return script.get_trmnl_data(masjid_id)
+    except Exception as e:
+        return {"error": str(e)}
